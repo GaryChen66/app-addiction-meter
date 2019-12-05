0bfb60f4b5dab259eb4d56bb1f3d6ae75ca8429c
Risingdeveloper007 <rising.hunter@yandex.com>
2019-12-06 02:02:02 +0800

    Updates

---
+++ precache-manifest.f0c9ecf400ab4d74af9b90c410e725f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d666d0c248cd6914ee5c490ddc08c76",
+    "url": "/app-addiction-meter/index.html"
+  },
+  {
+    "revision": "7122bb43d9642a10d60b",
+    "url": "/app-addiction-meter/static/css/main.e72f111f.chunk.css"
+  },
+  {
+    "revision": "5c07fd57bf0af77e07bb",
+    "url": "/app-addiction-meter/static/js/2.06fe0259.chunk.js"
+  },
+  {
+    "revision": "7122bb43d9642a10d60b",
+    "url": "/app-addiction-meter/static/js/main.4aa8c540.chunk.js"
+  },
+  {
+    "revision": "d0e4d72f19db566d3927",
+    "url": "/app-addiction-meter/static/js/runtime-main.9f6a72a9.js"
+  },
+  {
+    "revision": "8e899864f3b94508c9beb4e1a5d9f8ac",
+    "url": "/app-addiction-meter/static/media/get_icon.8e899864.png"
+  }
+]);
