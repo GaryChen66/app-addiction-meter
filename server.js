bd9573c1f084785276d7d5e8f4af50391266026c
Risingdeveloper007 <rising.hunter@yandex.com>
2019-12-19 20:15:19 +0800

    add test api

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Test API
 app.get("/api/send", (req, res) => {
     res.send({ express: "Hello From Express" });
 });
