e0f5201c91bbd4705a0e37af05da4e1b151e5bf6
risingdeveloper007 <rising.hunter@yandex.com>
2019-12-19 20:22:54 +0800

    google analytics to submit

--- client/src/components/submit_component.js
+++ client/src/components/submit_component.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import submit_logo from "../images/submit_logo.png";
 
+//Google Analytics to Submit page
 import ReactGA from "react-ga";
 ReactGA.initialize("UA-154738585-1");
 
