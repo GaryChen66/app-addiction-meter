5ca6409f9b8f68e37ee569cbc8c9f1040ddfecaf
risingdeveloper007 <rising.hunter@yandex.com>
2020-01-06 12:18:24 +0800

    remove footer

--- client/src/components/footer_component.js
+++
@@ -1,8 +0,0 @@
-import React, { Component, Fragment } from "react";
-
-class FooterComponent extends Component {
-    render() {
-        return <Fragment></Fragment>;
-    }
-}
-export default FooterComponent;
